0fdc574f820ac61d461ffe21748402463b07ffab
Huong <huongnguyenak96@gmail.com>
2019-03-22 19:26:42 +0700

    get - set

---
+++ d.js
@@ -0,0 +1,18 @@
+class Point{
+    constructor(x,y){
+        this.x = x 
+        this.y = y
+    }
+    get distance(){
+        return Math.sqrt(this.x*this.x + Math.pow(this.y,2))
+    }
+    set toaDoX(_toadox){
+        this.x = _toadox
+    }
+}
+const A = new Point(1,1)
+// console.log(A.distance()) //false
+// console.log(A.distance) 
+A.toaDoX = 3
+// A.x = 2
+console.log(A)
