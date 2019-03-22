47bc1f452e494a9cc1f2ad67436ddbdfa82b38f0
Huong <huongnguyenak96@gmail.com>
2019-03-22 18:25:13 +0700

    parameter

--- b.js
+++ b.js
@@ -2,9 +2,10 @@ class Person{
     constructor(ten, age, school){
         this.name = ten 
         this.age = age
+        this.school = school
     }
     goToSchool() {
-        return `Hôm nay bạn ${this.name} đi học`
+        return `Hôm nay bạn ${this.name} đi học tai ${this.school.name}`
     }
 }
 
@@ -15,3 +16,6 @@ class School{
     }
 }
 const school = new School('KPT','Cao Thang')
+
+const Teo = new Person('Teo', 21, {name : 'Khoa Pham Training'})
+console.log(Teo.goToSchool())
